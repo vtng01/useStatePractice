5e6c91e9cf933bd50cb16441a3c346af0ab8f408
vtn <42221407+vtng01@users.noreply.github.com>
2022-12-08 14:04:38 +0000

    Add files via upload

---
+++ src/components/UseState/index.js
@@ -0,0 +1,19 @@
+import './UseState.css'
+import {useState} from 'react'
+
+const UseState = () => {
+  const [theme, setTheme] = useState('light')
+  const [count, setCount] = useState(0)
+  return (
+    <div className={theme}>
+      <h1>UseState Component</h1>
+      <button onClick={() => setTheme('dark')}>Dark</button>
+      <button onClick={() => setTheme('light')}> Light</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>Increment</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>Decrement</button>
+    </div>
+  );
+};
+
+export default UseState;
